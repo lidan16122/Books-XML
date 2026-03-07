caeed2ed42212577ce86da97e15b01906b5a920f
Lidan Sassonker <lidan16122@gmail.com>
2026-03-07 18:22:32 +0200

    added env

--- client/client/src/app/books/books.service.ts
+++ client/client/src/app/books/books.service.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Book } from './books.models';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BooksService {
-  apiRoute = 'https://localhost:7044/api/books';
+  apiRoute = `${environment.apiUrl}api/books`;
 
   constructor(private http: HttpClient) { }
 
